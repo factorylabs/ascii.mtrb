28e5007e1b6f699566725a6db3e91154385f62d1
Jay Zeschin <jay.zeschin@factorylabs.com>
2010-10-06 17:05:36 -0600

    Fixed interval setting not to blow up on disconnect

--- telnet.js
+++ telnet.js
@@ -1,56 +1,66 @@
 var tcp = require("net"),
-	fs = require('fs'),
-	sys = require('sys');
-	
-	
-	function randInt(size){
+fs = require('fs'),
+sys = require('sys');
 
-	var rNum = Math.floor(Math.random()*size);
+function randInt(size) {
+    var rNum = Math.floor(Math.random() * size);
+    return rNum;
+}
 
-	return rNum;
+var videos = ['headphones',
+'powder',
+'snowflake',
+'on-time',
+'leader'];
 
-	}
-	
-	
-var videos = [ 'headphones',
-				'powder',
-				'snowflake',
-				'on-time',
-				'leader'];
-	
-var server = tcp.createServer(function (socket) {
-  socket.setEncoding("utf8");
-  socket.on("connect", function () {
-	sys.log('connected!');
-	
-	socket.write('\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n     *****                                          ASCII.FACTORYLABS.COM                                           *****     \r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n');
-	socket.write('\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n     *****     Resize your terminal to 180x55 or larger and strap yourself in.  Things are about to get Orange.     *****     \r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n');
+var server = tcp.createServer(function(socket) {
+    socket.setEncoding("utf8");
+		
+    socket.on("connect",
+    function() {
+        sys.log('connected!');
 
-		setTimeout(function() {
-		var dataChunks = [];
-		var videoName = videos[randInt(videos.length)];
-		var readStream = fs.createReadStream('videos/' + videoName + '.ascii', { 'flags': 'r', 'encoding': 'ascii', 'bufferSize': 55 * 180 });
-		readStream.on("data", function(str) {
-			dataChunks.push(str);
-		});
-	
-		var interval = setInterval(function(){
-			var chunk = dataChunks.shift();
-			if(chunk) {
-				socket.write(chunk);
-			} else {
-				clearInterval(interval);
-				setTimeout(function() {
-					socket.write('\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n     *****     Why not check out another video?  Telnet to ascii.factorylabs.com, port 5000     *****     \r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n');
-								socket.end();
-				}, 10000);
-			}
-		}, 40);
-	}, 10000);
+				socket.interval = null;
 
-  });
-  socket.on("end", function () {
-    socket.end();
-  });
+        socket.write('rnrnrnrnrnrnrnrn     *****                                          ASCII.FACTORYLABS.COM                                           *****     rnrnrnrnrnrnrnrn');
+        socket.write('rnrnrnrnrnrnrnrn     *****     Resize your terminal to 180x55 or larger and strap yourself in.  Things are about to get Orange.     *****     rnrnrnrnrnrnrnrn');
+
+        setTimeout(function() {
+            var dataChunks = [];
+            var videoName = videos[randInt(videos.length)];
+            var readStream = fs.createReadStream('videos/' + videoName + '.ascii', {
+                'flags': 'r',
+                'encoding': 'ascii',
+                'bufferSize': 55 * 180
+            });
+            readStream.on("data",
+            function(str) {
+                dataChunks.push(str);
+            });
+
+            socket.interval = setInterval(function() {
+                var chunk = dataChunks.shift();
+                if (chunk) {
+                    socket.write(chunk);
+                } else {
+                    clearInterval(interval);
+                    setTimeout(function() {
+                        socket.write('rnrnrnrnrnrnrnrn     *****     Why not check out another video?  Telnet to ascii.factorylabs.com, port 5000     *****     rnrnrnrnrnrnrnrn');
+                        socket.end();
+                    },
+                    10000);
+                }
+            },
+            40);
+        },
+        10000);
+    });
+
+    socket.on("end",
+    function() {
+        socket.end();
+        clearInterval(socket.interval);
+    });
+
 });
 server.listen(5000, "localhost");
